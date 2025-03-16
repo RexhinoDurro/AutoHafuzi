c53dc0f205a738e28d54f1c4dc41404ddcae2d0c
RexhinoDurro <rexhinodurro23@gmail.com>
2025-03-16 22:28:13 +0100

    changed path

--- backend/built.sh
+++ backend/built.sh
@@ -1,10 +1,12 @@
 set -o errexit
 
+# Change to the script's directory
+cd "$(dirname "$0")"
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then
+if [[ $CREATE_SUPERUSER ]]; then
     python manage.py createsuperuser --noinput
 fi
