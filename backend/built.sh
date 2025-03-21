3876e7e6032b0632ade52d1b816bf49f93892289
RexhinoDurro <rexhinodurro23@gmail.com>
2025-03-21 21:33:49 +0100

    makes and options and color

--- backend/built.sh
+++ backend/built.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py init_car_data
+python manage.py init_colors_options
 
 # if [[ $CREATE_SUPERUSER ]]; then
 #     python manage.py createsuperuser --noinput
